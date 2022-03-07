c300abca31cca3f6e54b12ebdb12a39005fbb494
Florient Manfo <manfoflorient@outlook.fr>
2022-03-07 09:37:56 +0100

    Create tabe user done:#6 Create table category done:#1

---
+++ Eshopam.Database/dbo/Tables/Category.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Category] (
+    [Id]     INT           IDENTITY (1, 1) NOT NULL,
+    [Name]   NVARCHAR (50) NOT NULL,
+    [UserId] INT           NOT NULL,
+    CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Category_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [IX_Category] UNIQUE NONCLUSTERED ([Name] ASC)
+);
+
---
+++ Eshopam.Database/dbo/Tables/User.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [Username] NVARCHAR (50) NOT NULL,
+    [Password] VARCHAR (100) NOT NULL,
+    [Fullname] NVARCHAR (50) NOT NULL,
+    [Role]     VARCHAR (50)  NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [IX_User] UNIQUE NONCLUSTERED ([Username] ASC)
+);
+
